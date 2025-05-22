6b74fc6b16542c37b7134853871e10c237f80f85
duo le Sun <1091282902@qq.com>
2025-05-22 13:46:03 +0800

    code for example

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![allow(unused_variables)]
+type File = String;
+
+fn open(f: &mut File) -> bool {
+    true
+}
+fn close(f: &mut File) -> bool {
+    true
+}
+
+#[allow(dead_code)]
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    unimplemented!()
+}
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    let mut f1 = File::from("f1.txt");
+    open(&mut f1);
+    //read(&mut f1, &mut vec![]);
+    close(&mut f1);
+}
